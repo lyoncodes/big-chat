4e60f79894cb4cfa448692feae2b3ce779d22403
Michael Lyon <lyoncodes@gmail.com>
2021-02-03 15:49:34 -0800

    scaffolds masterclass forum files

---
+++ src/components/PageHome.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+h1 hello from page home
+div(v-for="thread in threads" :key="thread.id")
+  h2 {{ thread.title }}
+  div(v-for="postId in thread.posts" :key="postId")
+    p {{userById(postById(postId).userId).name}}
+    p {{posts.find(p => p.id === postId).text}}
+</template>
+<script>
+import sourceData from '@/data.json'
+export default {
+  data () {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
+    }
+  },
+  methods: {
+    postById (postId) {
+      return this.posts.find(p => p.id === postId)
+    },
+    userById (userId) {
+      return this.users.find(p => p.id === userId)
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const noteApp = createApp(App)
+noteApp.component('NiceButton', {})
+noteApp.mount('#app')
