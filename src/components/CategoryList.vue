65fed9b138db5e4423244ff5ed4941f2f935080d
Michael Lyon <lyoncodes@gmail.com>
2021-04-02 15:05:08 -0700

    adds categories component and route for grouping threads by category

---
+++ src/components/CategoryList.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="forum-list col-full">
+  <ForumList
+    v-for="category in categories"
+    :key="category.id"
+    :forums="getForumsForCategory(category)"
+    :title="category.name"
+    :categoryId="category.id"
+  />
+
+</div>
+</template>
+<script>
+import ForumList from '@/components/ForumList'
+import sourceData from '@/data.json'
+export default {
+  components: {
+    ForumList
+  },
+  props: {
+    categories: {
+      required: true,
+      type: Array
+    }
+  },
+  methods: {
+    getForumsForCategory (category) {
+      return sourceData.forums.filter(forum => forum.categoryId === category.id)
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
